387bfeeed4835ba8cf59b50bae5a3b16da15a2ed
Casey Scheide <cscheide@ucsc.edu>
2014-01-14 21:58:42 -0800

    Added buttons to main menu
    Play, Settings, and Quit

--- Launch/Assets/MainMenu.cs
+++ Launch/Assets/MainMenu.cs
@@ -6,24 +6,45 @@ using System.Collections;
 /// </summary>
 public class MainMenu : MonoBehaviour {
 
+	/// <summary>
+	/// The background texture.
+	/// </summary>
 	public Texture backgroundTexture;
-	Rect viewRect = new Rect(0,0, Screen.width, Screen.height);
+
+	/// <summary>
+	/// The menu font.
+	/// </summary>
+	public Font menuFont;
+
+	private Rect viewRect = new Rect(0,0, Screen.width, Screen.height);
+	private Vector2 buttonSize = new Vector2(128, 64);
 
 	void OnGUI()
 	{
+		//GUIStyle myStyle = new GUIStyle();
+		//myStyle.font = menuFont;
+
+
 		//Show background texture
 		GUI.DrawTexture(viewRect, backgroundTexture, ScaleMode.ScaleAndCrop);
 
 		//"Play" button
-		if (GUI.Button(new Rect(128, Screen.height * 0.8f, Screen.width * 0.2f, Screen.height * 0.1f), "Begin Mission"))
+		if (GUI.Button(new Rect(128, Screen.height * 0.8f, buttonSize.x, buttonSize.y), "Launch"))
 		{
 
 		}
 
-		//"Play" button
-		if (GUI.Button(new Rect(128, Screen.height * 0.8f, Screen.width * 0.2f, Screen.height * 0.1f), "Options"))
+		//"Options" button
+		if (GUI.Button(new Rect(128 + buttonSize.x + 64, Screen.height * 0.8f, buttonSize.x, buttonSize.y), "Settings"))
 		{
-			
+
+		}
+
+		//"Quit" button
+		if (GUI.Button(new Rect(Screen.width - buttonSize.x - 128, Screen.height * 0.8f, buttonSize.x, buttonSize.y), "Quit"))
+		{
+			print ("Quitting...");
+			Application.Quit();
 		}
 	}
 }
