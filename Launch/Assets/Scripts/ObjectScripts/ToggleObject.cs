87100bff82ab91f49d9ada6201eb5c1a0e2c0cd0
smorning <SpencerMorn@gmail.com>
2014-01-24 15:10:51 -0800

    Added Toggle(d)Object, and ReceptacleObject

---
+++ Launch/Assets/Scripts/ObjectScripts/ToggleObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToggleObject : MonoBehaviour {
+	
+	/*
+	*	ToggledObject is dragged in via the GUI.
+	*	toggle() calls the ToggledObject's own method.
+	*	This code (and ToggledObject's) can be copied and moved into other scripts
+	*/
+	
+	public bool toggled = false;
+	public ToggledObject toggledObject;
+	
+	void OnMouseDown() {
+		toggle();
+	}
+	
+	
+	void toggle() {
+		toggledObject.toggle();
+		if (toggled) {
+			toggled = false;
+		} else {
+			toggled = true;
+		}
+	}
+}
