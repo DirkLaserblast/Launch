40428b4aa6d060b76449883ce2bf69af36c6e2d1
alwburke <alwburke@ucsc.edu>
2014-01-26 18:54:44 -0800

    Implemented Slider and On/Off
    Script for slider object and on/off object, need the "click away" close
    method for slider object that should be available soon.

---
+++ Launch/Assets/Scripts/OnOffObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnOffObject : MonoBehaviour {
+	private bool toggleOnOff = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (toggleOnOff) {
+			Camera.main.backgroundColor = Color.grey;
+		}
+		else {
+			Camera.main.backgroundColor = Color.magenta;
+		}
+	}
+
+	void OnMouseUp () {
+		toggleOnOff = !toggleOnOff;
+	}
+}
---
+++ Launch/Assets/Scripts/SliderObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliderObject : MonoBehaviour {
+	private float sliderValue;
+	private string sliderString;
+	private bool guiOn = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown () {
+		//SliderTypeObject();
+		guiOn = true;
+	}
+
+	private void OnGUI() {
+		//sets the value to a string so it can be displayed and displays it
+		sliderString = sliderValue.ToString ();
+		GUI.Label (new Rect (0, 0, 100, 100), sliderString);
+		//only display if clicked
+		if (guiOn) {
+			GUI.Box(new Rect(86, 90, 40, 120), "\t");
+			sliderValue = GUI.VerticalSlider (new Rect (100, 100, 50, 100), sliderValue, 1f, 0f);
+		}
+	}
+}
