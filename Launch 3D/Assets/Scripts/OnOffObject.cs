22f06cbd9313be03c97dfcde31305b52a570d925
Casey Scheide <cscheide@ucsc.edu>
2014-02-01 07:24:06 -0800

    Three rooms, lighting, adjustable asparagus lights, radio

---
+++ Launch 3D/Assets/Scripts/OnOffObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnOffObject : MonoBehaviour {
+	public bool toggleOnOff = true;
+	public AudioSource audioComponent;
+	public Light lightComponent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (toggleOnOff) {
+//			Camera.main.backgroundColor = Color.grey;
+//		}
+//		else {
+//			Camera.main.backgroundColor = Color.magenta;
+//		}
+	}
+
+	void OnMouseUp () {
+		toggleOnOff = !toggleOnOff;
+		if (audioComponent) audioComponent.mute = !audioComponent.mute;
+		if (lightComponent) lightComponent.enabled = !lightComponent.enabled;
+	}
+}
