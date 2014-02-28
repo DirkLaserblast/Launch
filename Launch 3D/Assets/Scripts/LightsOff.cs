d874aced9c804d7bc8e5f9d877e2683cad8449b2
smorning <SpencerMorn@gmail.com>
2014-02-28 13:15:21 -0800

    flashlight polished a little. Trurns on when it should.

--- Launch 3D/Assets/Scripts/LightsOff.cs
+++ Launch 3D/Assets/Scripts/LightsOff.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class LightsOff : MonoBehaviour {
 
 	public GameObject lights;
+	public GameObject Flashlight;
 	private DialogueTriggerScript DTS;
 
 	void Start () {
@@ -13,6 +14,8 @@ public class LightsOff : MonoBehaviour {
 	void OnTriggerEnter() {
 		if (DTS.read) {
 			lights.SetActive(false);
+			Flashlight.SetActive(true);
 		}
+
 	}
 }
