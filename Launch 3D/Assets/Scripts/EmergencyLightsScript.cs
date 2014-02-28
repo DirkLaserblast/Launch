b51730efcb2d767ac83511ec6b7cfc5f4722c358
Thomas <thcwong@ucsc.edu>
2014-02-28 13:54:27 -0800

    Revert "minor changes"
    This reverts commit e28080a4e37337349198cc2122fe716716bfca2a.
    
    also adds new scripts

---
+++ Launch 3D/Assets/Scripts/EmergencyLightsScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmergencyLightsScript : MonoBehaviour {
+	public GameObject lights;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(){
+		StartCoroutine (waittime ());
+	}
+	void startLights(){
+		lights.SetActive (true);
+	}
+	void closeLights(){
+		lights.SetActive (false);
+	}
+	IEnumerator waittime(){
+		while (true) {
+						yield return new WaitForSeconds (1.5f);
+						startLights ();
+						yield return new WaitForSeconds (1.5f);
+						closeLights ();
+				}
+
+	}
+}
