19e9e7d2fe2badc0160651f6f5366ad73ca0ff10
Casey Scheide <cscheide@ucsc.edu>
2014-03-05 15:16:43 -0800

    Added ejection system and status indicator

--- Launch 3D/Assets/ReceptacleScript.cs
+++ Launch 3D/Assets/ReceptacleScript.cs
@@ -11,16 +11,47 @@ public class ReceptacleScript : MonoBehaviour {
 	/// The hidden "locked in" item that is shown when the receptacle is active (has the item in it)
 	/// </summary>
 	public GameObject hiddenReceptacleItem;
-	public GameObject door;
+	public GameObject indicator;
 	public bool enabled = true;
+
+	private bool powered;
+
+	bool isPowered ()
+	{
+		return powered;
+	}
 
 	void OnTriggerEnter (Collider other)
 	{
+		print ("Triggered");
 		if (other.gameObject == receptacleItem && enabled)
 		{
-			door.GetComponent<DoorScript>().isLocked = false;
-			Destroy(receptacleItem);
+			receptacleItem.transform.position = hiddenReceptacleItem.transform.position;
+			receptacleItem.SetActive(false);
 			hiddenReceptacleItem.SetActive(true);
+			indicator.GetComponent<MeshRenderer>().material.color = Color.green;
+			powered = true;
 		}
+	}
+
+	void OnMouseDown ()
+	{
+		print ("Eject");
+		if (powered)
+		{
+			GetComponent<CapsuleCollider>().enabled = false;
+			hiddenReceptacleItem.SetActive(false);
+			receptacleItem.SetActive(true);
+			powered=false;
+			indicator.GetComponent<MeshRenderer>().material.color = Color.yellow;
+			StartCoroutine("wait", 3.0f);
+		}
+	}
+
+	IEnumerator wait (float time)
+	{
+		yield return new WaitForSeconds(time);
+		GetComponent<CapsuleCollider>().enabled = true;
+		indicator.GetComponent<MeshRenderer>().material.color = Color.red;
 	}
 }
