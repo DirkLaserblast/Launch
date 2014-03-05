8e6128bab966c6ee3bdaa4bec9b64476a8c4ca88
Casey Scheide <cscheide@ucsc.edu>
2014-03-05 13:34:40 -0800

    Made simple power cell puzzle that opens a door when the cell is placed in the receptacle

---
+++ Launch 3D/Assets/ReceptacleScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReceptacleScript : MonoBehaviour {
+
+	/// <summary>
+	/// The item that should be placed in the receptacle
+	/// </summary>
+	public GameObject receptacleItem;
+	/// <summary>
+	/// The hidden "locked in" item that is shown when the receptacle is active (has the item in it)
+	/// </summary>
+	public GameObject hiddenReceptacleItem;
+	public GameObject door;
+	public bool enabled = true;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject == receptacleItem && enabled)
+		{
+			door.GetComponent<DoorScript>().isLocked = false;
+			Destroy(receptacleItem);
+			hiddenReceptacleItem.SetActive(true);
+		}
+	}
+}
