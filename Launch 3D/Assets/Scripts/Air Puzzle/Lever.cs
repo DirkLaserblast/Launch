74735e7e2241036f31e29a24487431c3ce7a2fc0
smorning <SpencerMorn@gmail.com>
2014-03-13 18:42:13 -0700

    air puzzle changes

---
+++ Launch 3D/Assets/Scripts/Air Puzzle/Lever.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lever : MonoBehaviour {
+	public double timeLeft = 1f;
+	public bool started = false;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		if (started && timeLeft > 0) {
+			timeLeft -= Time.deltaTime;
+			transform.Rotate(0, 0, 120*Time.deltaTime);
+		}
+	}
+	
+	void OnMouseDown() {
+		started = true;
+	}
+}
