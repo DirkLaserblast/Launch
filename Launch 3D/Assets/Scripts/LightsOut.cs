99bd3d4101f3ede21d13b86ef60730343c4198c3
smorning <SpencerMorn@gmail.com>
2014-03-14 05:01:49 -0700

    light toggling/Merged with Thomas's lights

---
+++ Launch 3D/Assets/Scripts/LightsOut.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightsOut : MonoBehaviour {
+	public GameObject lights;
+	public GameObject redLights;
+	public GameObject flashlight;
+	public GameObject pointlight;
+	private float timer = 1f;
+	
+
+	void OnTriggerEnter() {
+		lights.SetActive (false);
+		StartCoroutine("TurnOn", timer);
+	}
+
+	IEnumerator TurnOn(float t) {
+		yield return new WaitForSeconds(t);
+		RedLightsOn();
+	}
+
+	void RedLightsOn() {
+		flashlight.SetActive (true);
+		pointlight.SetActive (true);
+		redLights.SetActive(true);
+	}
+}
