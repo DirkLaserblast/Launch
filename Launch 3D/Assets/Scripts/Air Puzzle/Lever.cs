37603811be5305fd63817d93478fa409d60df5b3
smorning <SpencerMorn@gmail.com>
2014-03-13 22:56:48 -0700

    added puzzles in to the scene. Plus some neat programmer art.

--- Launch 3D/Assets/Scripts/Air Puzzle/Lever.cs
+++ Launch 3D/Assets/Scripts/Air Puzzle/Lever.cs
@@ -3,20 +3,49 @@ using System.Collections;
 
 public class Lever : MonoBehaviour {
 	public double timeLeft = 1f;
+	private double baseTime = 1f;
+	private bool flipped = false;
 	public bool started = false;
+	public GameObject lockedDoor;
+	public GameObject unlockedDoor;
+	private DoorScript lockedDoorScript;
+	private DoorScript unlockedDoorScript;
+
 
 	void Start () {
-	
+		lockedDoorScript = lockedDoor.GetComponent<DoorScript> ();
+		unlockedDoorScript = unlockedDoor.GetComponent<DoorScript> ();
 	}
 	
 	void Update () {
 		if (started && timeLeft > 0) {
 			timeLeft -= Time.deltaTime;
-			transform.Rotate(0, 0, 120*Time.deltaTime);
+			if(!flipped) {
+				transform.Rotate(0, 0, 60*Time.deltaTime);
+			} else {
+				transform.Rotate(0, 0, -60*Time.deltaTime);
+			}
+		}
+		if (timeLeft <= 0) {
+			timeLeft = baseTime;
+			started = false;
+			Toggle();
 		}
 	}
 	
 	void OnMouseDown() {
 		started = true;
+	}
+
+	void Toggle() {
+		if (flipped) {
+			flipped = false;
+			lockedDoorScript.isLocked = true;
+			unlockedDoorScript.isLocked = false;
+		} else {
+			flipped = true;
+			lockedDoorScript.isLocked = false;
+			unlockedDoorScript.isLocked = true;
+		}
 	}
 }
--- Launch 3D/Assets/Scripts/PlugScript.cs
+++ Launch 3D/Assets/Scripts/PlugScript.cs
@@ -3,14 +3,42 @@ using System.Collections;
 
 public class PlugScript : MonoBehaviour {
 
-	private Vector3 oldPos;
+	public bool started = false;
+	private float duration = 0.8f;
+	public GameObject otherPlug;
+	public GameObject door;
+	private DoorScript doorScript;
+	private PlugScript otherPlugScript;
+	public bool pluggedIn;
 
-	void OnMouseDrag() {
-		print ("moo");
-		Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-		Vector3 delta = Vector3.zero;
-		delta.y = (pos.y - oldPos.y);
-		oldPos = pos;
-		transform.parent.transform.position += delta;
+	void Start() {
+		otherPlugScript = otherPlug.GetComponent<PlugScript> ();
+		doorScript = door.GetComponent<DoorScript> ();
+	}
+
+	void Update() {
+		if (started && duration > 0) {
+			transform.Translate(Vector3.right * Time.deltaTime * 0.2f);
+			duration -= Time.deltaTime;
+			if(duration <= 0) {
+				pluggedIn = true;
+				if(otherPlugScript.pluggedIn) {
+					doorScript.isLocked = false;
+				}
+			}
+		}
+	}
+
+
+	void OnMouseDown() {
+		started = true;
 	}
 }
+
+
+//print ("moo");
+//Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+//Vector3 delta = Vector3.zero;
+//delta.y = (pos.y - oldPos.y);
+//oldPos = pos;
+//transform.position += delta;
