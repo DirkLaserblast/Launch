548887fa0afa872715e7f732a16266b28fbb30da
smorning <SpencerMorn@gmail.com>
2014-04-02 16:36:37 -0700

    presentation build

--- Launch 3D/Assets/Scripts/LightsOut.cs
+++ Launch 3D/Assets/Scripts/LightsOut.cs
@@ -9,10 +9,22 @@ public class LightsOut : MonoBehaviour {
 	public AudioClip ventsPowerDown;
 	public GameObject flashlight;
 	public GameObject pointlight;
+	public AudioClip log1;
+	public AudioClip music;
+	public float timeout;
+	public GameObject player;
+	private bool playing = false;
 	private float timer = 1f;
+	private float logtimer = 4f;
 	
+	void Update() {
+		if (playing) {
+			transform.position = player.transform.position;
+		}
+	}
 
 	void OnTriggerEnter() {
+		playing = true;
 		lights.SetActive (false);
 		audio.PlayOneShot(ventsPowerDown);
 		Ventillation.SetActive(false);
@@ -22,15 +34,32 @@ public class LightsOut : MonoBehaviour {
 		}
 		StartCoroutine("TurnOn", timer);
 	}
-
+	
 	IEnumerator TurnOn(float t) {
 		yield return new WaitForSeconds(t);
 		RedLightsOn();
 	}
-
+	
 	void RedLightsOn() {
 		flashlight.SetActive (true);
 		pointlight.SetActive (true);
 		redLights.SetActive(true);
+		StartCoroutine("WaitToPlayLog", logtimer);
+	}
+	
+	IEnumerator WaitToPlayLog(float t) {
+		yield return new WaitForSeconds(t);
+		PlayLog ();
+	}
+	
+	void PlayLog() {
+		audio.PlayOneShot (log1);
+		audio.PlayOneShot (music);
+		StartCoroutine("TimeOut", timeout);
+	}
+	
+	IEnumerator TimeOut(float t) {
+		yield return new WaitForSeconds(t);
+		gameObject.SetActive (false);
 	}
 }
