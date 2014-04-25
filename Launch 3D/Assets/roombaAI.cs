107f57461e46d3d0da093d34c7a8ae94b0190b83
Casey Scheide <cscheide@ucsc.edu>
2014-04-24 22:38:42 -0700

    Roomba Puzzle is in the main scene, had to tweak it a lot, there's still a bug where it will attempt to avoid anything on the other side of the door, so for now there's nothing actually "jamming" the door

--- Launch 3D/Assets/roombaAI.cs
+++ Launch 3D/Assets/roombaAI.cs
@@ -5,6 +5,7 @@ public class roombaAI : MonoBehaviour {
 
 	public float objectAvoidRange = 2; //How close should the roomba get before it avoids an object
 	public float feelerAngle = 30; //Angle in degrees between the left and right feelers
+	public float velocityLimit = 1f;
 
 	private CarController carController;
 
@@ -15,37 +16,40 @@ public class roombaAI : MonoBehaviour {
 	
 	void Update ()
 	{
-		Vector3 forward = transform.TransformDirection(Vector3.forward);
-		Vector3 right = Quaternion.AngleAxis(30, Vector3.up) * forward;
-		Vector3 left = Quaternion.AngleAxis(-30, Vector3.up) * forward;
-
-		//Check feelers
-		bool leftFeeler = Physics.Raycast(transform.position, left, objectAvoidRange);
-		bool rightFeeler = Physics.Raycast(transform.position, right, objectAvoidRange);
-
-//		//If both side feelers are triggered, we're in a corner or wall, reverse and turn
-//		if (rightFeeler && leftFeeler)
-//		{
-//			carController.Move(-1f, -1f);
-//		}
-//		//If right feeler is triggered, turn left
-//		else if (rightFeeler)
-//		{
-//			carController.Move(-1f, 0.5f);
-//		}
-//		//If left feeler is triggered, turn right
-//		else if (leftFeeler)
-//		{
-//			carController.Move(1f, 0.5f);
-//		}
-//		//Otherwise drive straight
-//		else carController.Move(0f, 1f);
-
-		//Worse AI to help make puzzle possible, just reverse and turn on detection
-		if (rightFeeler || leftFeeler)
+		if (rigidbody.velocity.magnitude < velocityLimit)
 		{
-			carController.Move(-1f, -1f);
+			Vector3 forward = transform.TransformDirection(Vector3.forward);
+			Vector3 right = Quaternion.AngleAxis(30, Vector3.up) * forward;
+			Vector3 left = Quaternion.AngleAxis(-30, Vector3.up) * forward;
+			
+			//Check feelers
+			bool leftFeeler = Physics.Raycast(transform.position, left, objectAvoidRange);
+			bool rightFeeler = Physics.Raycast(transform.position, right, objectAvoidRange);
+			
+					//If both side feelers are triggered, we're in a corner or wall, reverse and turn
+					if (rightFeeler && leftFeeler)
+					{
+						carController.Move(-0.1f, -1f);
+					}
+					//If right feeler is triggered, turn left
+					else if (rightFeeler)
+					{
+						carController.Move(-0.5f, 0.5f);
+					}
+					//If left feeler is triggered, turn right
+					else if (leftFeeler)
+					{
+						carController.Move(0.5f, 0.5f);
+					}
+					//Otherwise drive straight
+					else carController.Move(0f, 1f);
+			
+//			//Worse AI to help make puzzle possible, just reverse and turn on detection
+//			if (rightFeeler || leftFeeler)
+//			{
+//				carController.Move(0f, -1f);
+//			}
+//			else carController.Move(0f, 1f);
 		}
-		else carController.Move(0f, 1f);
 	}
 }
