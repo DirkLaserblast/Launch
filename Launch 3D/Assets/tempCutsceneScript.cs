5f610344db6c1d88a7d447cba9f30c91fafa8c92
Casey Scheide <cscheide@ucsc.edu>
2014-04-27 22:41:51 -0700

    Added temporary triggers to power plant and area where the rocket will be (first plays a sound, second quits the game)

---
+++ Launch 3D/Assets/tempCutsceneScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tempCutsceneScript : MonoBehaviour {
+
+	public AudioClip success;
+	public GameObject secondTrigger;
+	public bool quitOnTrigger = false;
+
+	void OnTriggerEnter(Collider other)
+	{
+		audio.PlayOneShot(success);
+		gameObject.GetComponentInChildren<SphereCollider>().enabled = false;
+
+		if (secondTrigger) secondTrigger.SetActive(true);
+		if (quitOnTrigger) Application.Quit();
+	}
+}
