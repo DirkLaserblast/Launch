09881996e63912e2804ebdd42069df5cba924e99
Casey Scheide <cscheide@ucsc.edu>
2014-04-27 19:53:47 -0700

    Fixed Roomba AI so it will ignore any object named "Door Jammer", so something can lean on the door to "jam" it

--- Launch 3D/Assets/roombaAI.cs
+++ Launch 3D/Assets/roombaAI.cs
@@ -21,28 +21,32 @@ public class roombaAI : MonoBehaviour {
 			Vector3 forward = transform.TransformDirection(Vector3.forward);
 			Vector3 right = Quaternion.AngleAxis(30, Vector3.up) * forward;
 			Vector3 left = Quaternion.AngleAxis(-30, Vector3.up) * forward;
-			
+
+			RaycastHit hitLeft;
+			RaycastHit hitRight;
+
+
 			//Check feelers
-			bool leftFeeler = Physics.Raycast(transform.position, left, objectAvoidRange);
-			bool rightFeeler = Physics.Raycast(transform.position, right, objectAvoidRange);
+			bool leftFeeler = Physics.Raycast(transform.position, left, out hitLeft, objectAvoidRange) && hitLeft.collider.gameObject.name != "Door Jammer";
+			bool rightFeeler = Physics.Raycast(transform.position, right, out hitRight, objectAvoidRange) && hitRight.collider.gameObject.name != "Door Jammer";
 			
-					//If both side feelers are triggered, we're in a corner or wall, reverse and turn
-					if (rightFeeler && leftFeeler)
-					{
-						carController.Move(-0.1f, -1f);
-					}
-					//If right feeler is triggered, turn left
-					else if (rightFeeler)
-					{
-						carController.Move(-0.5f, 0.5f);
-					}
-					//If left feeler is triggered, turn right
-					else if (leftFeeler)
-					{
-						carController.Move(0.5f, 0.5f);
-					}
-					//Otherwise drive straight
-					else carController.Move(0f, 1f);
+			//If both side feelers are triggered, we're in a corner or wall, reverse and turn
+			if (rightFeeler && leftFeeler)
+			{
+				carController.Move(-0.1f, -1f);
+			}
+			//If right feeler is triggered, turn left
+			else if (rightFeeler)
+			{
+				carController.Move(-0.5f, 0.5f);
+			}
+			//If left feeler is triggered, turn right
+			else if (leftFeeler)
+			{
+				carController.Move(0.5f, 0.5f);
+			}
+			//Otherwise drive straight
+			else carController.Move(0f, 1f);
 			
 //			//Worse AI to help make puzzle possible, just reverse and turn on detection
 //			if (rightFeeler || leftFeeler)
