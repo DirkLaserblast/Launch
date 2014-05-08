43324cd9765a554ae4a20e94ad12d1b69e156ee5
Casey Scheide <cscheide@ucsc.edu>
2014-05-08 15:53:20 -0700

    Working on rocket and cutscene scripts

---
+++ Launch 3D/Assets/RocketScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketScript : MonoBehaviour {
+
+	public AudioClip liftOffSound;
+	public GameObject rocket;
+	public GameObject rocketCamera;
+	public Animator rocketAnimator;
+
+	//Play audio, switch cameras, start rocket launch animation
+	void OnTriggerEnter (Collider other)
+	{
+		audio.PlayOneShot(liftOffSound);
+		rocketCamera.SetActive(true);
+		Camera.main.gameObject.SetActive(false);
+	}
+}
