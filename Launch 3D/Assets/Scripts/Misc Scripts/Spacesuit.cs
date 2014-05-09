41a8fe8345039c5f09b6fe1c7cd950fb0f09ab11
smorning <SpencerMorn@gmail.com>
2014-05-09 04:58:49 -0700

    alpha testing build aklsdfjlakewnpgokenrg;alks

---
+++ Launch 3D/Assets/Scripts/Misc Scripts/Spacesuit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spacesuit : MonoBehaviour {
+
+	public GameObject carTrigger;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			carTrigger.SetActive(true);
+			gameObject.SetActive(false);
+		}
+	}
+}
