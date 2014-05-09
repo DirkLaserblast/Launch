70f3d2a9e511a16a241f3ab406163bed5039fd79
Casey Scheide <cscheide@ucsc.edu>
2014-05-09 00:46:55 -0700

    Added placeholder credits that reset the savegame, and fixed the positioning of the power plant trigger

---
+++ Launch 3D/Assets/creditsScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class creditsScript : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start ()
+	{
+		PlayerPrefs.DeleteAll();
+		yield return new WaitForSeconds(10);
+		//Wipe save
+		Application.LoadLevel(0);
+	}
+
+}
