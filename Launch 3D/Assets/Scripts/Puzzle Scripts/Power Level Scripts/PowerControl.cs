613ad5a9118158b99102b061c23a2679762f48bb
smorning <SpencerMorn@gmail.com>
2014-05-15 21:22:13 -0700

    Added instructions to diagnostics puzzle and now have it turn off lights in unpowered rooms.

--- Launch 3D/Assets/Scripts/Puzzle Scripts/Power Level Scripts/PowerControl.cs
+++ Launch 3D/Assets/Scripts/Puzzle Scripts/Power Level Scripts/PowerControl.cs
@@ -7,9 +7,18 @@ public class PowerControl : MonoBehaviour {
 	public PowerManager powerManager;
 	public List<OnOffSector> Sector = new List<OnOffSector>();
 	public List<DoorScript> Doors = new List<DoorScript>();
+	public GameObject Lights;
 	private bool modifiedLocks = false;
 	private bool modifiedPower = false;
 	private bool decrementedHere = false;
+
+	void Start() {
+		for(int i = 0; i < Sector.Count; i++) {
+			if (!Sector[i].isOn) {
+				TurnOffLights();
+			}
+		}
+	}
 	
 	public void Trigger() {
 		for(int i = 0; i < Sector.Count; i++) {
@@ -17,11 +26,13 @@ public class PowerControl : MonoBehaviour {
 				Sector[i].ToggleOff();
 				LockDoors();
 				IncrementPower();
+				TurnOffLights();
 			} else {
 				if(powerManager.CurrentPower > 0 || decrementedHere) {
 					UnlockDoors();
 					Sector[i].ToggleOn();
 					DecrementPower();
+					TurnOnLights();
 				}
 			}
 		}
@@ -48,6 +59,14 @@ public class PowerControl : MonoBehaviour {
 				}
 			}
 		}
+	}
+
+	void TurnOffLights() {
+		Lights.SetActive(false);
+	}
+
+	void TurnOnLights() {
+		Lights.SetActive(true);
 	}
 
 	void IncrementPower() {
