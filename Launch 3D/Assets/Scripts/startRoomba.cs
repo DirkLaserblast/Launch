970ed71fb4447d5a848ad633c06c0f487b3d1be0
Casey Scheide <cscheide@ucsc.edu>
2014-05-16 03:01:05 -0700

    AUGHAGHUWRULGHFHELIF

---
+++ Launch 3D/Assets/Scripts/startRoomba.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startRoomba : MonoBehaviour {
+
+	public GameObject roomba;
+
+	void OnTriggerEnter (Collider Other)
+	{
+		if (Other.tag == "Player")
+		{
+			roomba.SetActive(true);
+		}
+	}
+}
