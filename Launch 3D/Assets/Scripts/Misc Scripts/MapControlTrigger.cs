d1984d6e93e0c92e4fac52623db0615aa944c1ff
smorning <SpencerMorn@gmail.com>
2014-05-15 23:14:10 -0700

    added map into main scene

---
+++ Launch 3D/Assets/Scripts/Misc Scripts/MapControlTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapControlTrigger : MonoBehaviour {
+
+	public MapControl map;
+	public int sector;
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player") {
+			map.MovedToSector(sector);
+		}
+	}
+}
