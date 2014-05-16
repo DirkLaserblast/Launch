86428d8352f65b5b456f12ed07d9047a5d349ebd
smorning <SpencerMorn@gmail.com>
2014-05-15 22:36:44 -0700

    started work on map system for the pause menu

---
+++ Launch 3D/Assets/Scripts/Misc Scripts/MapControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapControl : MonoBehaviour {
+
+	public GameObject[] Arrows = new GameObject[8];
+	private int activeIndex = 0;
+
+	public void MovedToSector(int index) {
+		Arrows[index].SetActive(true);
+		Arrows[activeIndex].SetActive(false);
+		activeIndex = index;
+	}
+}
