cd1bc97b736c94a63c94653e2db708ef8899d39f
smorning <SpencerMorn@gmail.com>
2014-05-23 15:09:00 -0700

    added instructions and blinking in the diagnostics puzzle

---
+++ Launch 3D/Assets/Scripts/Misc Scripts/DeactivateOnTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeactivateOnTrigger : MonoBehaviour {
+
+	public GameObject logEntry;
+	
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player") {
+			logEntry.SetActive(false);
+		}
+	}
+}
