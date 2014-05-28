3d28be2198f5b13d287fa4cd6f0ee76b667545f1
Casey Scheide <cscheide@ucsc.edu>
2014-05-28 14:06:08 -0700

    Updated car camera so it's mouse controlled

---
+++ Launch 3D/Assets/DisableAntiClipScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableAntiClipScript : MonoBehaviour {
+
+	public ProtectCameraFromWallClip clipScript;
+
+	void OnTriggerEnter (Collider Other)
+	{
+		if (Other.tag == "Player")
+		{
+			clipScript.enabled = false;
+		}
+	}
+}
