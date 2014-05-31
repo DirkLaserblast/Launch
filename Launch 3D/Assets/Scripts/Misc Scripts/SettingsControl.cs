36e798d9d24b669e0758b9f933cb9c9e743328f9
smorning <SpencerMorn@gmail.com>
2014-05-31 16:16:25 -0700

    more work on the settings menu

---
+++ Launch 3D/Assets/Scripts/Misc Scripts/SettingsControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsControl : MonoBehaviour {
+
+	public dfDropdown AADropDown;
+	public dfDropdown QualityDropDown;
+	public dfCheckbox Vsync;
+	public dfSlider Volume;
+
+
+	void Awake () {
+		QualityDropDown.SelectedIndex = QualitySettings.GetQualityLevel ();
+		AADropDown.SelectedValue = "x" + QualitySettings.antiAliasing;
+		if (QualitySettings.vSyncCount == 0) {
+			Vsync.IsChecked = false;
+		} else {
+			Vsync.IsChecked = true;
+		}
+		Volume.Value = (int) (AudioListener.volume * 100);
+	}
+
+	public void UpdateAA() {
+		QualitySettings.antiAliasing = (int) Mathf.Pow(2, AADropDown.SelectedIndex);
+	}
+
+	public void UpdateQuality() {
+		QualitySettings.SetQualityLevel(QualityDropDown.SelectedIndex);
+	}
+
+	public void UpdateVsync() {
+		if(Vsync.IsChecked) {
+			QualitySettings.vSyncCount = 1;
+		} else {
+			QualitySettings.vSyncCount = 0;
+		}
+	}
+
+	public void UpdateVolume() {
+		AudioListener.volume = ((float)Volume.Value) / 100;
+	}
+}
