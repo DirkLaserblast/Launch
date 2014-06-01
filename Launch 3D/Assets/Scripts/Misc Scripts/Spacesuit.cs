a0a24bd6c9c4de36b69fd151d25b0176660e96e3
Casey Scheide <cscheide@ucsc.edu>
2014-05-31 19:22:04 -0700

    Made picking up the spacesuit open the airlock door (can't go through otherwise) and play a sound

--- Launch 3D/Assets/Scripts/Misc Scripts/Spacesuit.cs
+++ Launch 3D/Assets/Scripts/Misc Scripts/Spacesuit.cs
@@ -4,11 +4,20 @@ using System.Collections;
 public class Spacesuit : MonoBehaviour {
 
 	public GameObject carTrigger;
+	public GameObject door;
+	public GameObject doorLeft;
+	public GameObject doorRight;
+	public Collider collider;
+	public AudioSource doorAudio;
 
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "Player") {
+			doorAudio.Play();
 			carTrigger.SetActive(true);
 			gameObject.SetActive(false);
+			doorLeft.SetActive(false);
+			doorRight.SetActive(false);
+			collider.enabled = false;
 		}
 	}
 }
--- Launch 3D/Assets/StartMusicScript.cs
+++ Launch 3D/Assets/StartMusicScript.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class StartMusicScript : MonoBehaviour {
 
+	public Collider collider;
+
 	void OnTriggerEnter (Collider Other)
 	{
 		if (Other.tag == "Player")
 		{
 			audio.Play();
+			collider.enabled = false;
 		}
 	}
 }
